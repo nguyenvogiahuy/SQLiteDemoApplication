09fde4878981ef1149dfa4171e151e996cc07dbe
Admin <Admin@DESKTOP-24L225P>
2020-07-16 09:22:01 +0700

    first commit

---
+++ app/src/main/java/com/example/sqlitedemoapplication/SQLiteDemoApplicationActivity.java
@@ -0,0 +1,14 @@
+package com.example.sqlitedemoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SQLiteDemoApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sqlite_demo_application);
+    }
+}
